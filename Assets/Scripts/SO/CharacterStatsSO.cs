ae6336978e542aee8ae0d155ebc6250b399f7af5
cherry <cherry880807@gmail.com>
2026-05-13 22:13:50 +0800

    Add AnimationEvent - Attack

---
+++ Assets/Scripts/SO/CharacterStatsSO.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Data/Character Stats")]
+public class CharacterStatsSO : ScriptableObject
+{
+    public int maxHP = 100;
+    public int shield = 0;
+}
